5e17faaa19b1630701a752f46e9a8afcf4264167
Murat <mdenizalioglu@gmail.com>
2024-11-19 08:31:58 +0300

    Initialize project with Licensing Service and Config Server

---
+++ licensing-service/src/main/java/com/optimagrowth/license/model/License.java
@@ -0,0 +1,99 @@
+package com.optimagrowth.license.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.hateoas.RepresentationModel;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@Entity
+@Table(name="licenses")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class License extends RepresentationModel<License> {
+
+    @Id
+    @Column(name = "license_id", nullable = false)
+    private String licenseId;
+
+    private String description;
+
+    @Column(name = "organization_id", nullable = false)
+    private String organizationId;
+
+    @Column(name = "product_name", nullable = false)
+    private String productName;
+
+    @Column(name = "license_type", nullable = false)
+    private String licenseType;
+
+    @Column(name="comment")
+    private String comment;
+
+    public String getLicenseId() {
+        return licenseId;
+    }
+
+    public void setLicenseId(String licenseId) {
+        this.licenseId = licenseId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getOrganizationId() {
+        return organizationId;
+    }
+
+    public void setOrganizationId(String organizationId) {
+        this.organizationId = organizationId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getLicenseType() {
+        return licenseType;
+    }
+
+    public void setLicenseType(String licenseType) {
+        this.licenseType = licenseType;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    @Override
+    public String toString() {
+        return "License{" +
+                "licenseId='" + licenseId + '\'' +
+                ", description='" + description + '\'' +
+                ", organizationId='" + organizationId + '\'' +
+                ", productName='" + productName + '\'' +
+                ", licenseType='" + licenseType + '\'' +
+                ", comment='" + comment + '\'' +
+                '}';
+    }
+
+    public License withComment(String comment){
+        this.setComment(comment);
+        return this;
+    }
+}
